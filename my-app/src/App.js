c1e2db926ba84ce4269594563d6273740c39aeb3
PjHurtig <>
2023-08-09 12:25:03 +0000

    add handleclick function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      <StatefulGreeting greeting="stateful greeting" age="36" />
       <FunctionalGreetingWithProps greeting="nice to meet ya" name="PJ" age="35" />
     </div>
   );
-}
+} 
 
 export default App;
